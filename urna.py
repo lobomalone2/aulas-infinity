8dcdde27acd633831057ed5b541ac3cea422654d
lobomalone2 <lucaslobolone@gmail.com>
2024-10-29 18:02:12 -0300

    Condicionais para calcular percentual, tratamento de erros e resultado da eleição

--- urna.py
+++ urna.py
@@ -17,9 +17,14 @@ while True:
     
     voto = input('Digite um candidato pra votar')
 
+       
     if voto == 'sair':
         print('Encerrando votação')
         break
+
+    elif voto not in candidatos:
+        print('Candidato não encontrado tente novamente!')
+        continue
 
     candidatos[voto]['votos'] += 1
 
@@ -28,10 +33,45 @@ for cand, votos in candidatos.items():
     memoria = []
 
     for valor in votos.values():
+
         memoria.append(valor)
 
 
     print(f'Candidato {cand} - votos: {memoria[0]} ')
+
+if candidatos['A']['votos'] > candidatos['B']['votos'] and candidatos['A']['votos'] > candidatos['C']['votos']:
+
+    print('O candidato A Venceu')
+
+elif candidatos['B']['votos'] > candidatos['A']['votos'] and candidatos['B']['votos'] > candidatos['C']['votos']:
+
+    print(' O candidato B Venceu!')
+
+elif candidatos['C']['votos'] > candidatos['B']['votos'] and candidatos['C']['votos'] > candidatos['A']['votos']:
+
+    print('O Candidato C venceu!')
+
+
+total = candidatos['A']['votos'] + candidatos['B']['votos'] + candidatos['C']['votos']
+
+
+for cand, votos in candidatos.items():
+    
+    try:
+        percentual = float((votos['votos'] /total)) * 100
+
+    except ZeroDivisionError:
+
+        print('')
+        percentual = 0
+
+    print(f'Candidato A {cand} -  {percentual:.2f} % ')
+
+        
+
+
+
+
 
 
 
