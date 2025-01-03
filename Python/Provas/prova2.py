22e6fc8a0f58a35f22201997014e4d8a0b4414d3
lobomalone2 <lucaslobolone@gmail.com>
2025-01-03 13:53:00 -0300

    prova2

---
+++ Python/Provas/prova2.py
@@ -0,0 +1,16 @@
+
+nome = input('digite o nome')
+telefone = input('digite o telefone')
+email = input('digite o email')
+
+
+usuario = {
+    'nome': nome,
+    'telefone': telefone,
+    'email': email
+
+
+}
+
+
+print(usuario)
