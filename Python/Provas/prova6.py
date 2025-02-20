02a3f69ca44474d0b7c59b8aac34a91912d7a6f0
Lucas Lobo <lucaslobolone@gmail.com>
2025-02-20 14:43:18 -0300

    prova6

---
+++ Python/Provas/prova6.py
@@ -0,0 +1,15 @@
+import random
+
+
+def lancar_dados():
+    lista = [1, 2, 3, 4, 5,6]
+
+    numero1 = random.randint(1,6)
+
+    numero2 = random.randint(1,6)
+
+    return print(numero1 + numero2)
+
+
+lancar_dados()
+
